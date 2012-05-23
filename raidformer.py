71baad1aeafe794f852a03eac7909c9d030a674d
Benn Eichhorn <benn@roamz.com>
2012-05-23 17:51:40 +1000

    added abilty to create and mount RAID volumes from ec2 snapshots

--- raidformer.py
+++ raidformer.py
@@ -29,37 +29,40 @@ def get_options():
     parser = OptionParser(usage)
 
     parser.add_option("-a", "--attach",  action="store_true",
-            dest="attach", default=False, help="Do the volume creation and attachment.")
+        dest="attach", default=False, help="Do the volume creation and attachment.")
     parser.add_option("-c", "--count", action="store", type="int", \
-            dest="count", help="Number of EBS volumes ")
+        dest="count", help="Number of EBS volumes ")
     parser.add_option("-d", "--device", action="store", type="string", \
-            dest="device", default="/dev/sdf",  help="block device to start with")
+        dest="device", default="/dev/sdf",  help="block device to start with")
     parser.add_option("-f", "--filesystem", action="store", type="string", \
-            dest="filesystem", default="ext4", help="filesystem type")
+        dest="filesystem", default="ext4", help="filesystem type")
     parser.add_option("-l", "--logvol", action="store", type="string", \
-            dest="logvol", default="LogVolData", help="Logical Volume Name")
+        dest="logvol", default="LogVolData", help="Logical Volume Name")
     parser.add_option("-m", "--mountpoint", action="store", type="string", \
-            dest="mountpoint", help="Mountpoint")
+        dest="mountpoint", help="Mountpoint")
     parser.add_option("", "--md", action="store", type="string", \
-            dest="md_device", default="/dev/md0", help="md device name ")
+        dest="md_device", default="/dev/md0", help="md device name ")
     parser.add_option("-r", "--raidlevel", action="store", type="int", \
-            dest="raidlevel", help="RAID level")
+        dest="raidlevel", help="RAID level")
     parser.add_option("-s", "--size", action="store", type="int", \
-            dest="size", help="Size of EBS volumes ")
+        dest="size", help="Size of EBS volumes ")
     parser.add_option("-t", "--test",  action="store_true",
-            dest="test", default=False, help="Does a dry run of the mdadm lvm commands.")
+        dest="test", default=False, help="Does a dry run of the mdadm lvm commands.")
     parser.add_option("", "--tag", action="store", type="string", \
-            dest="tag", default="ebs_raid", help="Tag name for the ebs devices")
+        dest="tag", default="ebs_raid", help="Tag name for the ebs devices")
     parser.add_option("-v", "--volgroup", action="store", type="string", \
-            dest="volgroup", default="VolGroupData", help="Volume Group Name")
+        dest="volgroup", default="VolGroupData", help="Volume Group Name")
     parser.add_option("-w", "--wipe",  action="store_true",
-            dest="wipe", default=False, help="format the new filesystem")
+        dest="wipe", default=False, help="format the new filesystem")
+    parser.add_option("-S", "--snapshot",  action="store",
+        dest="snapshot", default=None,
+        help="Create volumes from csvlist of ebs snapshots (number of volumes must match count). Volumes will be assigned to devices in order")
+
     options, args = parser.parse_args()
 
     return options, args
 
 def initialize_raid( cmds, md_device, raidlevel, count, attached_devices ):
-
 
     cmds.append("echo  Y | mdadm  --verbose --create  %s --level=%s --chunk=256 --raid-devices=%s %s" % ( md_device, str(raidlevel), str(count), ' '.join(attached_devices) ) )
     cmds.append("mdadm --detail --scan >  /etc/mdadm.conf")
@@ -67,7 +70,7 @@ def initialize_raid( cmds, md_device, raidlevel, count, attached_devices ):
     cmds.append("pvcreate %s" % options.md_device )
     return cmds
 
-def initialize_filesystem(cmds, wipe, md_device, volgroup,logvol, format_cmds, filesystem, mountpoint):
+def initialize_filesystem(cmds, wipe, md_device, volgroup, logvol, format_cmds, filesystem, mountpoint):
 
     cmds.append("vgcreate %s %s" % ( volgroup, options.md_device  ) )
     cmds.append("lvcreate -l 100%%vg -n %s %s" % (logvol, volgroup) )
@@ -105,16 +108,29 @@ if not options.device in devices:
 
 if options.attach == True:
     if len(glob.glob(options.device)) > 0:
-      print "You already have devices with that start with %s." % options.device
+      print "You already have devices that start with %s." % options.device
       sys.exit(1)
     
 if os.path.exists(options.md_device):
   print "Device %s already exists." % options.md_device
   sys.exit(1)
 
+my_snapshots = []
+
+if options.snapshot:
+  my_snapshots = options.snapshot.split(',')
+  if len(my_snapshots) != options.count:
+    print "Value of --count=%s does not match the number of snapshot volumes provided %s" % (options.count, options.snapshot)
+    sys.exit(1)
+
+  if options.wipe:
+    print "Cowardly will not wipe volumes created from snapshots. Loose the --wipe option if you want to restore from snapshots."
+    sys.exit(1)
+
+
 my_devices = []
 
-for n in range(1,options.count +1):
+for n in range(1, options.count + 1):
     my_devices.append(options.device + str(n) )
 
 print my_devices
@@ -127,16 +143,20 @@ attached_devices = map(attached_name, my_devices)
 
 vol_ids = []
 
-if options.attach and not options.test:
+if (options.attach or options.snapshot) and not options.test:
 
-    for device in my_devices:
+    for key, device in enumerate(my_devices):
         print "Working on ", device
-        vol = ec2conn.create_volume(options.size, instance_data['placement']['availability-zone'])
+        if my_snapshots:
+          vol = ec2conn.create_volume(options.size, instance_data['placement']['availability-zone'], snapshot=my_devices[key])
+        else:
+          vol = ec2conn.create_volume(options.size, instance_data['placement']['availability-zone'])
         print "Created:", vol.id
         ec2conn.attach_volume(vol.id, instance_data['instance-id'], device)
         print "Attached:", vol.id
         vol_ids.append(vol.id)
-        vol.add_tag("Name", options.tag)     
+        vol.add_tag("Name", options.tag)
+
 
     
     for device in attached_devices:
